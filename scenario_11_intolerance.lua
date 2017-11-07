8ea5444a96187903936912da913763061e3ba35c
svalenelatis <tacomoyes123@gmail.com>
2017-11-07 16:27:02 -0700

    Create scenario_11_intolerance.lua

---
+++ scenario_11_intolerance.lua
@@ -0,0 +1,15 @@
+ -- Name: Intolerance
+ 
+ -- Description: Intolerance....yeah that's about it
+ 
+ -- Typer: Mission
+  
+  
+ function init()
+  
+  player = PlayerSpaceship():setFaction("Human Navy"):setTemplate("Atlantis")
+  
+ end
+ 
+ function update(delta)
+ end
