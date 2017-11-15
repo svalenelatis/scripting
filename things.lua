db28dec33395c3f994e7317003ed9c32722564fb
svalenelatis <tacomoyes123@gmail.com>
2017-11-14 21:24:50 -0700

    Create things.lua

---
+++ things.lua
@@ -0,0 +1,22 @@
+template = ShipTemplate():setName("S.S. Bossman"):setClass("Frigate", "Diplomatic Gunship"):setModel("battleship_destroyer_1_upgraded"):setType(“playership”)
+template:setDescription([[The S.S. Bossman is considered an intergalacticdestroyercruiserdiplomatictransportlightheavyadvancedgun
+missileshipplatformarcon5000systemartemis/apollo hybrid ship. It’s high range and accuracy with phasers and missiles make it a formidable opponent]])
+template:setRadarTrace("radar_adv_striker.png")
+template:setHull(100)
+template:setShields(150, 150)
+template:setSpeed(60, 16, 10)
+template:setWarpSpeed(700)
+template:setCombatManeuver(250, 250)
+template:setBeam(0, 40, 170, 1200.0, 1.0, 1)
+template:setBeam(1, 40, 190, 1200.0, 1.0, 1)
+template:setBeam(2, 40, 10, 1200.0, 1.0, 1)
+template:setBeam(3, 40, 350, 1200.0, 1.0, 1)
+template:setTubes(9, 20.0)
+--set tube directions
+
+
+
+
+template:setWeaponStorage("Homing", 25)
+template:setWeaponStorage("Emp", 10)
+template:setWeaponStorage("Nuke", 10)
